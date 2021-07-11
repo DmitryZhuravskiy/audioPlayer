5590b1e4b8da38944cdd8d294d5ef4349833629e
Dmitry Zhuravskiy <dmitry.zhuravskiy@gmail.com>
2021-07-11 21:54:25 +0300

    working version

--- js/main.js
+++ js/main.js
@@ -1,41 +1,42 @@
-const player = document.querySelector('.player'),
-    playBtn = document.querySelector('.play'),
-    prevBtn = document.querySelector('.prev'),
-    nextBtn = document.querySelector('.next'),
-    audio = document.querySelector('.audio'),
-    progressContainer = document.querySelector('.pregress__container'),
-    progress = document.querySelector('.progress'),
-    title = document.querySelector('.title'),
-    cover = document.querySelector('.cover__img'),
-    imgSrc = documnet.querySelector('.img__src')
-        
+const player = document.querySelector('.player')
+const playBtn = document.querySelector('.play')
+const prevBtn = document.querySelector('.prev')
+const nextBtn = document.querySelector('.next')
+const audio = document.querySelector('.audio')
+const progressContainer = document.querySelector('.progress__container')
+const progress = document.querySelector('.progress')
+const title = document.querySelector('.title')
+const cover = document.querySelector('.cover__img')
+const imgSrc = document.querySelector('.img__src')
+
 // Название песен
-const songs = ['Dance With The Dead - Robeast', 'Psykosonik - Panik Control', 'Scooter - Move Your Ass!']
+const songs = ['Dance With The Dead - Robeast', 'Psykosonik - Panic Сontrol', 'Scooter - Move Your Ass!']
 
 // Песня по умолчанию
 let songIndex = 2
 
 //Init
 function loadSong(song) {
-    title.InnerHTML = song
+    title.innerHTML = song
     audio.src = `./audio/${ song }.mp3`
-    cover.src = `./image/cover__img${songIndex + 1}.jpg`
+    cover.src = `./image/cover__img${ songIndex + 1 }.jpg`
+    console.log(songIndex)
 }
 
-loadSong(songs[songsIndex])
+loadSong(songs[songIndex]);
 
 //Play
 function playSong() {
     player.classList.add('play')
-    cover.playList.add('active')
+    cover.classList.add('active')
     imgSrc.src = `./image/pause.png`
     audio.play()
 }
 
-//Play
+//Pause
 function pauseSong() {
     player.classList.remove('play')
-    cover.classList.remover('active')
+    cover.classList.remove('active')
     audio.pause()
 }
 playBtn.addEventListener('click', () => {
@@ -46,3 +47,45 @@ playBtn.addEventListener('click', () => {
         playSong()
     }
 })
+
+function nextSong() {
+    songIndex++
+
+    if (songIndex > songs.length - 1) {
+        songIndex = 0
+    }
+
+    loadSong(songs[songIndex])
+    playSong()
+}
+
+nextBtn.addEventListener('click', nextSong)
+
+function prevSong() {
+    songIndex--
+
+    if (songIndex < 0) {
+        songIndex = songs.length - 1
+    }
+
+    loadSong(songs[songIndex])
+    playSong()
+}
+prevBtn.addEventListener('click', prevSong)
+
+function updateProgress(e) {
+    const { duration, currentTime } = e.srcElement
+    const progressPercent = (currentTime / duration) * 100
+    progress.style.width = `${progressPercent}%`
+}
+audio.addEventListener('timeupdate', updateProgress)
+
+function setProgress(e) {
+    const width = this.clientWidth
+    const clickX = e.offsetX
+    const duration = audio.duration
+    audio.currentTime = (clickX / width) * duration
+    console.log( clickX, width)
+}
+progressContainer.addEventListener('click', setProgress)
+audio.addEventListener('ended', nextSong)
